8b0dec53eb41e860268b8fbdd117790251cb8728
LEEjgsp <whdtjq1gh@naver.com>
2022-12-31 16:42:22 +0900

    클론코당완료

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded",
     function(e) {
         let FasFaCog = document.querySelector("#id i")
         let Idtext = document.querySelector("#id span")
-        let ProfileChange = document.querySelector("#profile info button")
+        
+        FasFaCog.addEventListener("click",
+            function(e){
+                Idtext.textContent = prompt("새로운 아이디를 입력하세요.")
+            }
+        )
+
+        let ProfileChange = document.querySelector("#profile_info button")
         let UserInfo = document.querySelector("#userInfo")
         let Summary = document.querySelector("#summary")
         let profileDetail = document.querySelector("#profileDetail")
         let changing = false
-
-        FasFaCog.addEventListener("click",
-            function(e){
-                Idtext.textContent = prompt("새로운 아이디를 입력하세요.")
-            })
         
         ProfileChange.addEventListener("click",
             function(e) {
@@ -49,5 +51,21 @@ document.addEventListener("DOMContentLoaded",
                 }        
             }
         )
+
+        let ProfilePic = document.querySelector("#profile_pic")
+
+        ProfilePic.addEventListener("mouseover",
+            function(e) {
+                e.target.style.border = "solid red"
+            }
+        )
+        ProfilePic.addEventListener("mouseleave",
+            function(e){
+                e.target.style.border = "none"
+            })
+        ProfilePic.addEventListener("click",
+            function(e){
+                ProfilePic.setAttribute("src", prompt("이미지 url을 입력해주세요."))
+            })
     }
 )
